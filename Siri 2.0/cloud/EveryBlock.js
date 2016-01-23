5778256a286eaeb926149a506520e0cbdf5be4e5
Ryan D'souza <dsouzarc@gmail.com>
2016-01-23 06:04:47 -0500

    More objects

--- Siri 2.0/cloud/EveryBlock.js
+++ Siri 2.0/cloud/EveryBlock.js
@@ -1,4 +1,5 @@
 module.exports = {
+
     getMetros: function(options) {
         return Parse.Cloud.httpRequest({
             method: "GET",
@@ -7,6 +8,28 @@ module.exports = {
         }).then(function(httpResponse) {
             return httpResponse.data;
             if (options && options.success) {
+            }
+        }, function(httpResponse) {
+            if (options && options.error) {
+                options.error(httpResponse);
+            }
+        });
+    },
+
+    getSchemas: function(metroName, options) {
+        return Parse.Cloud.httpRequest({
+            method: "GET",
+            url: "https://api.everyblock.com/content/" + metroName + "/topnews/?token=fd5f0d8fc74fd048fbb811ee29215be5fef04274",
+            body: {},
+        }).then(function(httpResponse) {
+            console.log(httpResponse);
+            console.log("https://api.everyblock.com/content/" + metroName + "/schemas?token=fd5f0d8fc74fd048fbb811ee29215be5fef04274");
+            console.log("STILL HERE: " + httpResponse.status);
+            console.log("STILL HERE: " + httpResponse);
+            console.log("HERE: " + httpResponse.text);
+            console.log("2HERE: " + httpResponse.data);
+            return httpResponse.data;
+            if(options && options.success) {
             }
         }, function(httpResponse) {
             if (options && options.error) {
--- Siri 2.0/cloud/main.js
+++ Siri 2.0/cloud/main.js
@@ -4,11 +4,9 @@ var Metro = Parse.Object.extend("Metro");
 
 Parse.Cloud.define("getMetros", function(request, response) {
     everyBlock.getMetros().then(function(httpResponse) {
-        
         for(var i = 0; i < httpResponse.length; i++) {
             var metro = new Metro();
             var metroJSON = httpResponse[i];
-            
             for(property in metroJSON) {
                 metro.set(property, metroJSON[property]);
             }
@@ -20,7 +18,6 @@ Parse.Cloud.define("getMetros", function(request, response) {
                 }
             });
         }
-        
         response.success("Got metros");
         }, function(httpResponse) {
             console.error(httpResponse);
@@ -29,19 +26,45 @@ Parse.Cloud.define("getMetros", function(request, response) {
     );
 });
 
-Parse.Cloud.beforeSave("Metros", function(request, response) {
-    var query = new Parse.Query(Metro);
+Parse.Cloud.define("getSchemas", function(request, response) {
+
+    everyBlock.getSchemas(request.params.metro_name).then(function(httpResponse) {
+        var Schema = new Parse.Object.extend("Schemas");
+        for(var i = 0; i < httpResponse.length; i++) {
+            var schema = new Schema();
+            var schemaJSON = httpResponse[i];
+            for(property in schemaJSON) {
+                schema.set(property, schemaJSON[property]);
+            }
+
+            schema.save(null, {
+                success: function(result) {
+                }, 
+                error:function(schema, error) {
+                    console.log(error.message);
+                }
+            });
+        }
+        response.success("Got schemas");
+    }, function(httpResponse) {
+        console.error(httpResponse);
+        response.error("Something went wrong");
+    });
+});
+
+
+Parse.Cloud.beforeSave("Metro", function(request, response) {
+    var query = new Parse.Query("Metro");
     query.equalTo("metro_name", request.object.get("metro_name"));
     query.first({
-      success: function(object) {
-        if (object) {
-          response.error("Metro already exists");
-        } else {
-          response.success();
+        success: function(object) {
+            if (object) {
+                response.error("Metro already exists");
+            } else {
+                response.success();
+            }
+        }, error: function(error) {
+            response.error("Could not validate uniqueness for this BusStop object.");
         }
-      },
-      error: function(error) {
-        response.error("Could not validate uniqueness for this BusStop object.");
-      }
     });
 });
